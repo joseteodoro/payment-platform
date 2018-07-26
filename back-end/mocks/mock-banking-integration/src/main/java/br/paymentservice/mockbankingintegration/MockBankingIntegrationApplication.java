862898642fa8c2da5237fc38d18bc7fc3ef4f647
joseteodoro <jteodoro.dev@gmail.com>
2018-07-26 11:35:18 -0300

    adding basic springboot service to mock banking integrations

---
+++ back-end/mocks/mock-banking-integration/src/main/java/br/paymentservice/mockbankingintegration/MockBankingIntegrationApplication.java
@@ -0,0 +1,12 @@
+package br.paymentservice.mockbankingintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockBankingIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockBankingIntegrationApplication.class, args);
+	}
+}
