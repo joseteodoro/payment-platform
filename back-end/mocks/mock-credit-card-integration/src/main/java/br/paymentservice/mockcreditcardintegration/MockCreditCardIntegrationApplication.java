83f081b53b516d3ec8540864e2ad32805f8b59da
joseteodoro <jteodoro.dev@gmail.com>
2018-07-26 11:35:36 -0300

    adding basic springboot service to mock plastic integrations

---
+++ back-end/mocks/mock-credit-card-integration/src/main/java/br/paymentservice/mockcreditcardintegration/MockCreditCardIntegrationApplication.java
@@ -0,0 +1,12 @@
+package br.paymentservice.mockcreditcardintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockCreditCardIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockCreditCardIntegrationApplication.class, args);
+	}
+}
---
+++ back-end/mocks/mock-credit-card-integration/src/test/java/br/paymentservice/mockcreditcardintegration/MockCreditCardIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package br.paymentservice.mockcreditcardintegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockCreditCardIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
